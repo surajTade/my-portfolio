a5b766eb746b113364efc17e4a038e0404199328
surajTade <101720866+surajTade@users.noreply.github.com>
2024-08-23 16:13:09 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,53 +1,53 @@
 import "./home.css";
-import profile from "../../assets/home1.jpg";
+import profile from "../../assets/me_v3.png";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
 import Typed from "typed.js";
 import { useEffect, useRef } from "react";
 
 const Home = () => {
-	const autoText = useRef();
+  const autoText = useRef();
 
-	useEffect(() => {
-		const typed = new Typed(autoText.current, {
-			strings: ["Student!", "Engineer!", "Designer!", "Developer!"],
-			typeSpeed: 100,
-			backSpeed: 150,
-			loop: true,
-		});
+  useEffect(() => {
+    const typed = new Typed(autoText.current, {
+      strings: ["Engineer!", "Developer!", "Student!", "Designer!"],
+      typeSpeed: 100,
+      backSpeed: 150,
+      loop: true,
+    });
 
-		return () => {
-			// Destroy Typed instance during cleanup to stop animation
-			typed.destroy();
-		};
-	}, []);
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
 
-	return (
-		<section className="home section grid">
-			<img src={profile} className="home-img" alt="" />
-			<div className="home-content">
-				<div className="home-data">
-					<h1 className="home-title">
-						I'm Suraj Tade. <span ref={autoText}></span>
-					</h1>
+  return (
+    <section className="home section grid">
+      <img src={profile} className="home-img" alt="" />
+      <div className="home-content">
+        <div className="home-data">
+          <h1 className="home-title">
+            I'm Suraj Tade. <span ref={autoText}></span>
+          </h1>
 
-					<p className="home-description">
-						I am currently pursuing B.Tech. in Computer Engineering with honours
-						in Deep Learning at PCCOE, Pune. I'm an organised person ready to
-						learn and improve upon skills.
-					</p>
+          <p className="home-description">
+            Completed B.Tech. in Computer Engineering along with honours in Deep
+            Learning from PCCOE, Pune. I'm an organised person ready to learn
+            and improve upon skills.
+          </p>
 
-					<Link to="/about" className="btn">
-						More About Me
-						<span className="btn-icon">
-							<FaArrowRight />
-						</span>
-					</Link>
-				</div>
-			</div>
-			<div className="color-block"></div>
-		</section>
-	);
+          <Link to="/about" className="btn">
+            More About Me
+            <span className="btn-icon">
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div className="color-block"></div>
+    </section>
+  );
 };
 
 export default Home;
